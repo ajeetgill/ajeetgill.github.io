63439f41fc03d60632af079a063ac43963b498e7
Ajeet Gill <ajeetshergill@gmail.com>
2020-05-31 22:21:57 +0530

    404 page
    Pulled the page from github.
    Modified it and now pushing it to origin

--- js/main.js
+++ js/main.js
@@ -39,21 +39,21 @@ class TxtRotate {
         }, delta);
     }
 }
-  
-  
-  window.onload = function() {
+
+
+window.onload = function () {
     var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
         // console.log(elements.length);
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
     }
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
-  };
+};
